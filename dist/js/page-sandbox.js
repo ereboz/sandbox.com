29bc2cca5d367e6a6aa9cf63deb9416c265ad1b9
ereboz <ereboz8@gmail.com>
2018-06-05 14:26:57 -0700

    footer save

--- dist/js/page-sandbox.js
+++ dist/js/page-sandbox.js
@@ -1,7 +1,7 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
-	// jQuery('li:eq(0)').css("border", "1px solid blue");
+// 	// jQuery('li:eq(0)').css("border", "1px solid blue");
 
-	jQuery('li:eq(2)').prevAll().css("border", "1px dotted green");
+// 	jQuery('li:eq(2)').prevAll().css("border", "1px dotted green");
 
-});
+// });
